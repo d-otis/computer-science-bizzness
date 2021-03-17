aa910ada694cbb038423b7e51edfa6cbd75d58d2
d-otis <dan@dan-foley.com>
2021-03-16 20:05:13 -0400

    define functions for selectionSort()

--- selectionSort.js
+++ selectionSort.js
@@ -1,5 +1,29 @@
 function minAndRemove(array) {
+  let min = array[0]
+  let minIndex = 0
 
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i]
+      minIndex = i
+    }
+  }
+
+  array.splice(minIndex, 1)
+  return min
 }
 
-minAndRemove([ 5, 6, -1, 1, 3 ])
+
+function selectionSort(array) {
+  let newMin
+  let sorted = []
+
+  while (array.length !== 0) {
+    newMin = minAndRemove(array)
+    sorted.push(newMin)
+  }
+
+  return sorted
+}
+
+selectionSort([ 5, 6, -1, 1, 3 ])
