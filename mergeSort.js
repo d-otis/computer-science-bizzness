74c58fb81cb6f1ac178c8189547fe5ebc562c22b
d-otis <dan@dan-foley.com>
2021-03-23 18:35:30 -0400

    add flatiron findMindAndRemove()

--- mergeSort.js
+++ mergeSort.js
@@ -1,5 +1,16 @@
 let costOfItemsAtTraderJoes =  [1, 2, 6, 7, 8]
 let costOfItemsAtWholeFoods =  [3, 4, 5, 9, 10]
+
+function findMinAndRemove(firstHalf, secondHalf){
+  let minfirstHalf = firstHalf[0]
+  let minsecondHalf = secondHalf[0]
+ 
+  if(minfirstHalf < minsecondHalf){
+    return firstHalf.shift()
+  } else {
+    return secondHalf.shift()
+  }
+}
  
 function merge(first, second) {
   let sorted = []
